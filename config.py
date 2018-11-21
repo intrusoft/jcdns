b912d3bcdd43572fcf2469afe7357639c53b5948
JC Smith <js@intrusoft.com>
2018-11-21 14:40:06 -0800

    only read the zone config when its changed

--- config.py
+++ config.py
@@ -5,18 +5,27 @@ Removed Cassandra backend, replaced with simple json format for now
 """
 
 import json
+import os
 
 configfile = 'example.json'
 
 class Config:
+   def __init__(self):
+       self.running_config = None
+       self.config_time = 0
 
    def getconfig(self, domainname):
-       # todo: pass in config file
-       with open(configfile) as f:
-           data = json.load(f)
 
-       if domainname in data:
-           return data[domainname]
+       if self.config_time < os.stat(configfile).st_mtime:
+           print "Loading new config from file"
+           self.config_time  = os.stat(configfile).st_mtime 
+           with open(configfile) as f:
+               self.running_config = json.load(f)
+       else:
+           print "Using cached config"
+
+       if domainname in self.running_config:
+           return self.running_config[domainname]
        else:
            return None
 
